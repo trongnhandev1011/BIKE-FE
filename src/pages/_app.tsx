7d07e7e70c983e6df03645bd6cbee3955d08536e
ZERO-94 <70008423+ZERO-94@users.noreply.github.com>
2023-02-15 21:09:40 +0700

    move notification to container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,57 +8,29 @@ import { Provider } from "react-redux";
 import { AuthProvider } from "@containers/AuthProvider";
 import { fetcher } from "@utils/common";
 import { WrapperLayout } from "@layouts/WrapperLayout";
-import { useEffect, useRef, useState } from "react";
-import SockJS from "sockjs-client";
-import { Stomp } from "@stomp/stompjs";
-import { notification } from "antd";
+import { NotificationProvider } from "@containers/NotificationProvider";
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
-  const [lastPong, setLastPong] = useState(null);
-  const count = useRef(0);
-
-  useEffect(() => {
-    const socket = new SockJS("http://52.74.214.224:8080/ws");
-    const stompClient = Stomp.over(socket);
-    stompClient.connect({}, () => {
-      console.log("connected");
-      stompClient.subscribe(
-        "/user/e7f506b0-2147-408c-ba9d-102e7a8f03be/notifications",
-        (mess) => {
-          count.current += 1;
-          console.log(count.current);
-          notification.info({
-            //@ts-ignore
-            message: JSON.parse(mess.body).title,
-            //@ts-ignore
-            description: JSON.parse(mess.body).body,
-          });
-        }
-      );
-    });
-  }, []);
 
   return (
     <Provider store={store}>
       <AuthProvider>
-        <SWRConfig
-          value={{
-            fetcher: fetcher,
-            onError: (err) => {
-              console.error(err);
-            },
-          }}
-        >
-          <WrapperLayout>
-            <Component {...pageProps} />
-          </WrapperLayout>
-        </SWRConfig>
+        <NotificationProvider>
+          <SWRConfig
+            value={{
+              fetcher: fetcher,
+              onError: (err) => {
+                console.error(err);
+              },
+            }}
+          >
+            <WrapperLayout>
+              <Component {...pageProps} />
+            </WrapperLayout>
+          </SWRConfig>
+        </NotificationProvider>
       </AuthProvider>
     </Provider>
   );
-}
-
-function NotificationProvider({ children }: { children: any }) {
-  return <>{children()}</>;
 }
